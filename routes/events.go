c392081ba5f987a36bd53ab10e0bd1dfd6dbd43c
levi-w-lucy <reachfan169@gmail.com>
2025-07-09 08:31:32 -0500

    Adding functionality for updating and deleting events.

--- routes/events.go
+++ routes/events.go
@@ -21,7 +21,7 @@ func getEvents(context *gin.Context) {
 func getEvent(context *gin.Context) {
 	eventId, err := strconv.ParseInt(context.Param("eventId"), 10, 64)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID"})
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID."})
 		return
 	}
 
@@ -52,4 +52,61 @@ func createEvent(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+}
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("eventId"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID."})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not find event with that ID."})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully!"})
+}
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("eventId"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not find event with that ID."})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete the event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Sucessfully deleted event."})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -6,4 +6,6 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:eventId", getEvent)
 	server.POST("/events", createEvent)
+	server.PUT("/events/:eventId", updateEvent)
+	server.DELETE("/events/:eventId", deleteEvent)
 }
